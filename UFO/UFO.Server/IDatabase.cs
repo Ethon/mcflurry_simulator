4654240687a148b7286b270c91ac9ac9be1cbcdc
Chris138 <christian.holzinger86@gmail.com>
2015-10-19 23:24:45 +0200

    Added IDatabase
    Added IDatabase, which is the interface for different
    database-implementations

---
+++ UFO/UFO.Server/IDatabase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UFO.Server {
+    public interface IDatabase {
+        DbCommand CreateCommand(string sql);
+        int DeclareParamater(DbCommand command, string name, DbType type);
+        void DefineParameter(DbCommand command, string name, DbType type, object value);
+        void SetParameter(DbCommand command, string name, object value);
+
+        IDataReader ExecuteReader(DbCommand command);
+        int ExecuteNonQuery(DbCommand command);
+    }
+}
