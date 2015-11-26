70f9a1a8a945077a170df757e83b69a39f44cf2f
Florian Erler <ethon@ethon.cc>
2015-11-26 19:29:15 +0100

    First commit for ArtistManagement

---
+++ UFO/UFO.Commander/ViewModel/CategoryListViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using UFO.Server;
+using UFO.Server.Data;
+
+namespace UFO.Commander.ViewModel {
+    class CategoryListViewModel {
+        private ICategoryService categoryService;
+
+        public CategoryListViewModel(ICategoryService categoryService) {
+            this.categoryService = categoryService;
+            Categories = new ObservableCollection<CategoryViewModel>();
+            UpdateCategories();
+        }
+
+        public ObservableCollection<CategoryViewModel> Categories {
+            get; set;
+        }
+
+        public void UpdateCategories() {
+            Categories.Clear();
+            foreach(var cat in categoryService.GetAllCategories()) {
+                Categories.Add(new CategoryViewModel(cat));
+            }
+        }
+    }
+}
