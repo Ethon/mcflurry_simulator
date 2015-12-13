990788a258b4f298a44eeaa7e0bbd31ff4f0750f
Florian Erler <ethon@ethon.cc>
2015-12-13 12:44:30 +0100

    Added DayProgramExporter

---
+++ UFO/UFO.Commander/DayProgramExporter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UFO.Commander {
+    public interface IDayProgramExporter {
+        void exportDayProgram(DateTime date, string outFile);
+    }
+
+    public class DayProgramHtmlExporter : IDayProgramExporter {
+        public void exportDayProgram(DateTime date, string outFile) {
+            throw new NotImplementedException();
+        }
+    }
+}
