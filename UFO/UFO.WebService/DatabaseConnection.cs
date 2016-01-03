f4392c3f0fc81dbeb21fb7727d52d0bc8d8befc9
Chris138 <christian.holzinger86@gmail.com>
2016-01-03 13:11:34 +0100

    Added DatabaseConnection

---
+++ UFO/UFO.WebService/DatabaseConnection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using UFO.Server;
+
+namespace UFO.WebService {
+    public class DatabaseConnection {
+
+        private static IDatabase database;
+
+        public static IDatabase GetDatabase() {
+            if (database == null) {
+                string providerName = ConfigurationManager.ConnectionStrings["DefaultConnectionString"].ProviderName;
+                if (providerName == "Mysql.Data.MysqlClient") {
+                    database = new MYSQLDatabase(ConfigurationManager.ConnectionStrings["DefaultConnectionString"].ConnectionString);
+                } else {
+                    throw new NotSupportedException("Database not supported");
+                }
+            }
+
+            return database;
+        }
+    }
+    
+}
