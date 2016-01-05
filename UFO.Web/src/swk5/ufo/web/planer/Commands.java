85ba8e8455a321c9dba9b451b7eff14472ef1673
Chris138 <christian.holzinger86@gmail.com>
2016-01-05 19:47:24 +0100

    Added AllArtists and AllPerformances Site

---
+++ UFO.Web/src/swk5/ufo/web/planer/Commands.java
@@ -0,0 +1,12 @@
+package swk5.ufo.web.planer;
+
+public class Commands {
+	public static final String GO_HOME = "GoHomeEvent";
+	public static final String BROWSE_ARTISTS = "BrowseAllArtistsEvent";
+	public static final String BROWSE_PERFORMANCES = "BrowseAllPerformancesEvent";
+	public static final String ARTICLE_DETAIL = "BrowseArticleDetailsEvent";
+	public static final String PUT_ARTICLE = "PutArticleIntoCartEvent";
+	public static final String BROWSE_ARTICLE_SEARCH = "BrowseAllArticlesSearchEvent";
+	public static final String REMOVE_ARTICLE = "RemoveArticleFromCartEvent";
+	public static final String CHECKOUT = "CheckoutEvent";
+}
