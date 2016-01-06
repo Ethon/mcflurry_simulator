a88fb10ffc3f9ccd181bd0fceafad9777f38aebe
Florian Erler <ethon@ethon.cc>
2016-01-06 15:50:00 +0100

    Added Venues page

--- UFO.Web/src/swk5/ufo/web/viewmodel/Commands.java
+++ UFO.Web/src/swk5/ufo/web/viewmodel/Commands.java
@@ -4,6 +4,7 @@ public class Commands {
 	public static final String GO_HOME = "GoHomeEvent";
 	public static final String BROWSE_ARTISTS = "BrowseAllArtistsEvent";
 	public static final String BROWSE_PERFORMANCES = "BrowseAllPerformancesEvent";
+	public static final String BROWSE_VENUES = "BrowseAllVenuesEvent";
 	public static final String ARTICLE_DETAIL = "BrowseArticleDetailsEvent";
 	public static final String PUT_ARTICLE = "PutArticleIntoCartEvent";
 	public static final String BROWSE_ARTICLE_SEARCH = "BrowseAllArticlesSearchEvent";
